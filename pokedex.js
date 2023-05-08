ed22c8162a2ac045c19124d5442d1c5d7bb20208
AlexSichitiu <asichitiu@gmail.com>
2023-05-08 16:50:31 -0700

    Added event listener for clicks on page buttons. Began implementing event listener for pokemon card detail button.

--- pokedex.js
+++ pokedex.js
@@ -3,7 +3,7 @@ const MAX_PAGES_DISPLAYED = 5;
 
 async function producePokemon(currentPage, pokemon){
     currentPage < 1 ? currentPage = 1 : currentPage;
-    currentPage > Math.ceil(pokemon.length / CARDS_PER_PAGE) ? Math.ceil(pokemon.length / CARDS_PER_PAGE) : currentPage
+    currentPage > Math.ceil(pokemon.length / CARDS_PER_PAGE) ? Math.ceil(pokemon.length / CARDS_PER_PAGE) : currentPage;
     currentPokemon = pokemon.slice((currentPage - 1) * CARDS_PER_PAGE, currentPage * CARDS_PER_PAGE);
     
     $('#pokemonCards').empty();
@@ -11,7 +11,7 @@ async function producePokemon(currentPage, pokemon){
         var res = await axios.get(givenPokemon.url);
         var info = res.data;
         $('#pokemonCards').append(`
-            <div class="card" pokemonName="${info.name}">
+            <div class="card pokemonCard" pokemonName="${info.name}">
                 <h4>${info.name.toUpperCase()}</h4>
                 <img src="${info.sprites.front_shiny}" alt="${info.name}" style="height: 200px; width: 200px;">
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pokemonModal">
@@ -23,12 +23,14 @@ async function producePokemon(currentPage, pokemon){
 }
 
 async function updatePages(currentPage, pageCount){
+    currentPage < 1 ? currentPage = 1 : currentPage;
+    currentPage > pageCount ? currentPage = pageCount : currentPage
     $('#pages').empty();
     var boundLength = Math.floor(MAX_PAGES_DISPLAYED / 2);
     var lowerPage = Math.max(currentPage - boundLength, 1);
     var upperPage = Math.min(currentPage + boundLength, pageCount);
     for (var i = lowerPage; i <= upperPage; i++){
-        $('#pages').append(`<button class="btn btn-primary page ml-1 numberedButtons" value="${i}">${i}</button>`)
+        $('#pages').append(`<button class="btn btn-primary page ml-1 pageButton" value="${i}">${i}</button>`)
     }
 }
 
@@ -37,9 +39,23 @@ const setup = async () => {
     let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
     var pokemon = response.data.results;
     var currentPage = 1;
-    producePokemon(currentPage, pokemon);
     var pageCount = Math.ceil(pokemon.length / CARDS_PER_PAGE);
+    producePokemon(currentPage, pokemon);
     updatePages(currentPage, pageCount);
+
+    $('body').on('click', '.pageButton', async (e) => {
+        currentPage = parseInt(e.target.value);
+        producePokemon(currentPage, pokemon);
+        updatePages(currentPage, pageCount);
+    });
+
+    $('body').on('click', '.pokemonCard', async (e) => {
+        var name = $(this).attr('pokemonName');
+        console.log(name);
+        var res = await axios.get('https://pokeapi.co/api/v2/pokemon/' + name);
+        $('.modal-title').empty().append(name.toUpperCase());
+        $('.modal-body').empty()
+    });
 }
 
 $(document).ready(setup);
